222f0f43e970fc716a95328df755d53069430ac4
Amir El-Sibaie <19491001+aelsibaie@users.noreply.github.com>
2022-07-12 03:21:43 -0400

    make better

--- mov3.py
+++ mov3.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 
 # local vars
+LIMIT = 30 * 1e+9 # 30gb
 output_dir = "/home/pi/timelapse"
 input_dir = "/home/pi/pictures"
 
@@ -40,6 +41,14 @@ if __name__ == "__main__":
     ffcall.append('-i')
     ffcall.append(os.path.join(srcfolder, "img_?????.jpg"))
 
+
+    ffcall.append('-c:v')
+    ffcall.append('libx264')
+
+    ffcall.append('-crf')
+    ffcall.append('17')
+
+
     # overwrite output
     ffcall.append('-y')
 
@@ -58,4 +67,27 @@ if __name__ == "__main__":
 
     print(' '.join(ffcall))
     sys.stdout.flush()
-    subprocess.call(ffcall, stdout=sys.stdout, stderr=sys.stderr)
+    completed_process = subprocess.run(ffcall, stdout=sys.stdout, stderr=sys.stderr)
+
+    if (completed_process.returncode != 0):
+        print("ffmpeg failed, keeping pictures")
+    else:
+        print("ffmpeg worked, deleting old pictures")
+        subprocess.run(['rm', '-rf', srcfolder])
+
+    size = 0
+    for ele in os.scandir(output_dir):
+        size += os.path.getsize(ele)
+
+    while size > LIMIT:
+        oldest_file = sorted([ "/home/pi/timelapse/"+f for f in os.listdir("/home/pi/timelapse/")], key=os.path.getctime)[0]
+        print("removing oldest file", oldest_file)
+        os.remove(oldest_file)
+        size = 0
+        for ele in os.scandir(output_dir):
+            size += os.path.getsize(ele)
+
+
+    print(size)
+
+    print("finished processing timelapse")
--- simple-cam.py
+++ simple-cam.py
@@ -5,8 +5,14 @@ import datetime
 import shutil
 import socket
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(filename='simple-cam.log', level=logging.DEBUG)
+#logging.basicConfig(filename='simple-cam.log', level=logging.DEBUG)
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler("simple-cam.log", maxBytes=1000000, backupCount=1)
+logger.addHandler(handler)
+
 
 RESOLUTION = "1920x1080"
 SCALE_TOGGLE = False
@@ -35,39 +41,39 @@ semaphore_fn = "/home/pi/simple-cam/running.txt"
 
 def email_error_report(error_str):
     os.remove(semaphore_fn)
-    logging.critical(error_str)
-    logging.info("emailing report...")
+    logger.critical(error_str)
+    logger.info("emailing report...")
     # wait to import because it takes a long time and is only needed for errors
-    import gmail
-    gmail.send_message(gmail.service, "amir.elsibaie@gmail.com", "simple-cam.py Critical Error", error_str)
+    # import gmail
+    # gmail.send_message(gmail.service, "amir.elsibaie@gmail.com", "simple-cam.py Critical Error", error_str)
     exit()
 
 
 if __name__ == "__main__":
-    logging.info("starting simple-cam.py at " + str(datetime.datetime.now()))
+    logger.info("starting simple-cam.py at " + str(datetime.datetime.now()))
 
     total, used, free = shutil.disk_usage("/")
     freep = free / total
-    logging.debug("Total: %d GiB" % (total // (2**30)))
-    logging.debug("Used: %d GiB" % (used // (2**30)))
-    logging.debug("Free: %d GiB" % (free // (2**30)))
+    logger.debug("Total: %d GiB" % (total // (2**30)))
+    logger.debug("Used: %d GiB" % (used // (2**30)))
+    logger.debug("Free: %d GiB" % (free // (2**30)))
 
     if(freep < (HDD_SPACE_THRESHOLD / 100)):
         error_str = str(round((freep * 100), 2)) + "% remaining hard drive space too low on " + socket.gethostname()
         email_error_report(error_str)
     else:
-        logging.info(str(round((freep * 100), 2)) + "% remaining hard drive space at acceptable levels")
+        logger.info(str(round((freep * 100), 2)) + "% remaining hard drive space at acceptable levels")
 
     datestr = time.strftime("%Y%m%d-%H%M%S")  # 20120515-155045
     dest_path = os.path.join(img_root, datestr)
     if not os.path.exists(dest_path):
-        logging.info("creating jpg dir " + dest_path)
+        logger.info("creating jpg dir " + dest_path)
         os.mkdir(dest_path)
 
     # make semaphore file
     open(semaphore_fn, 'a').close()
     time.sleep(1)
-    logging.info("loop started at " + str(datetime.datetime.now()))
+    logger.info("loop started at " + str(datetime.datetime.now()))
 
     imcount = 0
     # run forever and wait to be killed by cronjob.
@@ -75,60 +81,24 @@ if __name__ == "__main__":
     error_message = "No such file or directory"
     while os.path.exists(semaphore_fn):
         remote_path = os.path.join(dest_path, "img_{:05}.jpg".format(imcount))
-        logging.info("sending remoteshoot at " + str(datetime.datetime.now()))
+        logger.info("sending remoteshoot at " + str(datetime.datetime.now()))
         if SCALE_TOGGLE == True:
-            completed_process = subprocess.run(["fswebcam", "-r", RESOLUTION, "--no-banner", "--set", BRIGHTNESS, "--set", CONTRAST, "--set", SATURATION, "--set", HUE, "--scale", SCALE, remote_path], capture_output=True)
+            completed_process = subprocess.run(["fswebcam", "-r", RESOLUTION, "-S", "10", "--no-banner", "--set", BRIGHTNESS, "--set", CONTRAST, "--set", SATURATION, "--set", HUE, "--scale", SCALE, "--flip", "v", remote_path], capture_output=True)
         else:
-            completed_process = subprocess.run(["fswebcam", "-r", RESOLUTION, "--no-banner", "--set", BRIGHTNESS, "--set", CONTRAST, "--set", SATURATION, "--set", HUE, remote_path], capture_output=True)
-        logging.debug(str(completed_process))
+            completed_process = subprocess.run(["fswebcam", "-r", RESOLUTION, "-S", "10", "--no-banner", "--set", BRIGHTNESS, "--set", CONTRAST, "--set", SATURATION, "--set", HUE, "--flip", "v", remote_path], capture_output=True)
+        logger.debug(str(completed_process))
         if (completed_process.returncode != 0) or (error_message in str(completed_process.stderr)):
             email_error_report(str(completed_process))
         imcount += 1
-        logging.info("remoteshoot success. sleeping " + str(INTERVAL) + " seconds starting at: " + str(datetime.datetime.now()))
+        logger.info("remoteshoot success. sleeping " + str(INTERVAL) + " seconds starting at: " + str(datetime.datetime.now()))
         time.sleep(INTERVAL)
 
     # ok here we are done with the loop. Turn off the backlight.
-    logging.info("loop ended at " + str(datetime.datetime.now()))
+    logger.info("loop ended at " + str(datetime.datetime.now()))
 
     # wait for last download to finish
     time.sleep(10)
-    
-    
-    logging.info("starting daily report at " + str(datetime.datetime.now()))
-    ## start daily report section
-    my_list = sorted(os.listdir(dest_path))
-    SPLIT = 20
-    indexes = []
-    for i in range(SPLIT - 1):
-        indexes.append(int((len(my_list) / 20) * (i + 1)))
 
-    logging.info("making gif at " + str(datetime.datetime.now()))    
-    from PIL import Image, ImageDraw
-    frames = []
-    for index in indexes:
-        myImage = Image.open(os.path.join(dest_path, my_list[index])).resize((320,180))
-        myDrawing = ImageDraw.Draw(myImage)
-        myDrawing.text((28, 36), my_list[index][4:-4], fill=(255, 0, 0))
-
-        frames.append(myImage)
-
-    frames[0].save("daily_summary.gif", format="GIF", append_images=frames, save_all=True, duration=5000/(SPLIT-1), loop=0, )
-    
-    logging.info("emailing summary at " + str(datetime.datetime.now()))    
-    import gmail
-    today = datetime.date.today()
-    subj = "simple-cam.py Daily Report - {:%d, %b %Y}".format(today)
-
-    body = ""
-    body += "Remaining hard drive space: " + str(round(freep*100, 2)) + "%\n"
-    body += "Minimum required hard drive space: " + str(HDD_SPACE_THRESHOLD) + "%\n\n"
-    body = body + ("Total: %d GiB\n" % (total // (2**30)))
-    body = body + ("Used: %d GiB\n" % (used // (2**30)))
-    body = body + ("Free: %d GiB" % (free // (2**30)))
-    
-    gmail.send_message(gmail.service, "amir.elsibaie@gmail.com", subj, body, attachments=["/home/pi/simple-cam/daily_summary.gif"])
-
-
-    logging.info("job finished at " + str(datetime.datetime.now()))
+    logger.info("job finished at " + str(datetime.datetime.now()))
 
     exit()
